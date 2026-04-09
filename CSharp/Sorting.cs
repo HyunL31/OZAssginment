ef902d322b214474d29865d8f11b60dd6236ffde
이주현 <juhyun040100@gmail.com>
2026-04-09 16:42:55 +0900

    [과제] Day 16 - 정렬 알고리즘

---
+++ CSharp/Sorting.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Reflection;
+
+namespace DailyAssignmentCSharp
+{
+    internal class Sorting
+    {
+        static void InsertionSort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int key = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+
+                arr[j + 1] = key;
+            }
+        }
+
+        static void BubbleSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - 1 - i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        static void SelectionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int minIdx = i;
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIdx])
+                    {
+                        minIdx = j;
+                    }
+                }
+
+                int temp = arr[i];
+                arr[i] = arr[minIdx];
+                arr[minIdx] = temp;
+            }
+        }
+
+        static void QuickSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(arr, left, right);
+
+                QuickSort(arr, left, pivot - 1);
+                QuickSort(arr, pivot + 1, right);
+            }
+        }
+
+        static int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[right];
+            int idx = left - 1;
+
+            for (int i = left; i < right; i++)
+            {
+                if (arr[i] < pivot)
+                {
+                    idx++;
+
+                    int temp1 = arr[idx];
+                    arr[idx] = arr[i];
+                    arr[i] = temp1;
+                }
+            }
+
+            int temp2 = arr[idx + 1];
+            arr[idx + 1] = arr[right];
+            arr[right] = temp2;
+
+            return idx + 1;
+        }
+
+        static void MergeSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int mid = (left + right) / 2;
+
+                MergeSort(arr, left, mid - 1);
+                MergeSort(arr, mid + 1, right);
+
+                Merge(arr, left, mid, right);
+            }
+        }
+
+        static void Merge(int[] arr, int left, int mid, int right)
+        {
+            int[] temp = new int[right -  left + 1];
+
+            int i = left;
+            int j = mid + 1;
+            int k = 0;
+
+            while (i <= mid && j <= right)
+            {
+                if (arr[i] <= arr[j])
+                {
+                    temp[k++] = arr[i++];
+                }
+                else
+                {
+                    temp[k++] = arr[j++];
+                }
+            }
+
+            while (i <= mid)
+            {
+                temp[k++] = arr[i++];
+            }
+
+            while (j <= right)
+            {
+                temp[k++] = arr[j++];
+            }
+
+            for (int idx = 0; idx < temp.Length; idx++)
+            {
+                arr[left + idx] = temp[idx];
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 6, 9, 8, 1, 3, 2, 4 };
+
+            MergeSort(arr, 0, arr.Length - 1);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
