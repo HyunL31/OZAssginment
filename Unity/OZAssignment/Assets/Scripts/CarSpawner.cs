2bb4181da82860d17315681da3fd417f9871bed2
이주현 <juhyun040100@gmail.com>
2026-04-22 15:41:34 +0900

    [과제] Day 25 - 프리팹 인스턴스, 키 입력

---
+++ Unity/OZAssignment/Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    public GameObject carPrefab;
+    public Transform spawnPos;
+    public GameObject carRoot;
+
+    //void OnEnable()
+    //{
+    //    // 실체화 후 부모 설정
+    //    var car = Instantiate(carPrefab, spawnPos);
+    //    car.transform.SetParent(carRoot.transform);
+    //}
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnPrefab();
+        }
+    }
+
+    private void SpawnPrefab()
+    {
+        // 실체화와 함께 부모 설정
+        Instantiate(carPrefab, spawnPos.position, Quaternion.identity, carRoot.transform);
+    }
+}
